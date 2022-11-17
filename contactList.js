07d365d5e8da6ac13950f1614e8f039c2e76c096
jucezar <juliocezar.m.a@gmail.com>
2022-11-17 00:17:03 -0300

    Configurado arquivo contactList.js

--- contactList.js
+++ contactList.js
@@ -1,3 +1,25 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 
-export default class ContactList extends LightningElement {}
+// importando referencias dos campos
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+
+// importando a função getContacts() da classe ContactController, onde fica tambem
+// disponiveis devido ao @AuraEnable.
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+
+// criando um array de objetos onde seus atributos são 'puxados' do import
+const COLUMNS = [
+    { label: 'First Name', fieldName: FIRSTNAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Last Name', fieldName: LASTNAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Email', fieldName: EMAIL_FIELD.fieldApiName, type: 'text' }
+];
+
+
+export default class ContactList extends LightningElement {
+
+columns = COLUMNS; 
+// usado o @wire com a função getContacts o este resultado é armazenado na variável contacts
+@wire(getContacts) contacts;
+}
